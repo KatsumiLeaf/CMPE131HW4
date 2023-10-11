a26d121f0f8ebbcc103362553ffec279187c5558
Benjamin Lim <144379490+KatsumiLeaf@users.noreply.github.com>
2023-10-10 21:25:42 -0700

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,6 +1,6 @@
 def reverse_sort_dictionary(dict_input):
     reverse_items = sorted(dict_input.items(), key=lambda x: x[0], reverse=True)
 
-    result = [(name, data[0]) for name, data in reverse_items)]
+    result = [(name, data[0]) for name, data in reverse_items]
 
     return result
