d19275d7e4fb6898f257ab4f7d79990716c9e8de
Benjamin Lim <144379490+KatsumiLeaf@users.noreply.github.com>
2023-10-12 19:35:47 -0700

    Update cacti.py
    Code change

--- cacti.py
+++ cacti.py
@@ -1,22 +1,25 @@
 def cacti_number(array_input):
     if not array_input:
         return 0
-    
+
     row = len(array_input)
     col = len(array_input[0])
 
-    def cacti(i, j):
+    def can_plant_cacti(i, j):
         return (
-            array_input[i][j] == 1
-            and (i == 0  or array_input[i - 1][j] == 0)
+            array_input[i][j] == 0
+            and (i == 0 or array_input[i - 1][j] == 0)
             and (j == 0 or array_input[i][j - 1] == 0)
+            and (i == row - 1 or array_input[i + 1][j] == 0)
+            and (j == col - 1 or array_input[i][j + 1] == 0)
         )
-    
-    cacti_count = 0
+
+    open_spot = 0
 
     for i in range(row):
         for j in range(col):
-            if cacti(i, j):
-                cacti_count += 1
+            if can_plant_cacti(i, j):
+                array_input[i][j] = 1
+                open_spot += 1
 
-    return cacti_count
+    return open_spot
